336f7c8f23be72ef7a395b2086c2dd568e44093f
German <german.glumov@gmail.com>
2022-12-22 20:12:00 +0300

    one

---
+++ 1/Program.cs
@@ -0,0 +1,18 @@
+﻿void FillArray(double[,] arr)
+{
+    Random rand = new Random();
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rand.Next(1, 10) + rand.NextDouble();
+            arr[i, j] = Math.Round(arr[i, j], 2);
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = new double[3,4];
+FillArray(array);
+
